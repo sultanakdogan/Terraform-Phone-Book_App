e569d53ce7ab5de8f5c7ec21ab1ce7e4794277a1
Sultan Akdogan <sultanakdogan026@gmail.com>
2023-03-21 01:04:21 +0000

    Project is completed

---
+++ output.tf
@@ -0,0 +1,6 @@
+output "dns-name" {
+    value = "http://${aws_alb.app-lb.dns_name}"
+}
+output "websiteurl" {
+    value = aws_route53_record.phonebook.name
+}
---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxxxxxxx"
+}
+variable "git-name" {
+  default = "james-clarusway"
+}
+variable "key-name" {
+  type = "string"
+  default = "xxx"
+}
+variable "hosted-zone" {
+  default = "sultanakdogan.co.uk"
+}
